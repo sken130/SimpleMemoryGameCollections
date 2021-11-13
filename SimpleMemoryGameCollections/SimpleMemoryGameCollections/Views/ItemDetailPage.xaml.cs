7354ecf96d61f4c61e1bad8d66a6daa6ef83bfd2
Ken <sken130@gmail.com>
2021-11-13 11:51:40 +0800

    Add project files.

---
+++ SimpleMemoryGameCollections/SimpleMemoryGameCollections/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SimpleMemoryGameCollections.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SimpleMemoryGameCollections.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
